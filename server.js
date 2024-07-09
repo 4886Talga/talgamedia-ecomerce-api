247d71cfae354a63fa0b1973dcae602260261f09
Samir Talic <4886talga@gmail.com>
2024-07-09 11:22:09 +0200

    remove server.js

--- server.js
+++
@@ -1,9 +0,0 @@
-const http = require("http");
-const app = require("./app");
-
-const port = process.env.PORT || 3001;
-const server = http.createServer(app);
-
-app.listen(port, () => {
-  console.log("Server running on port 3001");
-});
